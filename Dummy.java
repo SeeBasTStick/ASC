c2fb6913f126095f4ea200d7b35e31cf084b9c21
sesto104 <izgmadminno1_hhu>
2019-05-04 22:54:08 +0200

    added some more test changes

--- Dummy.java
+++ Dummy.java
@@ -5,6 +5,7 @@ public class Dummy
 	public static void main(String args[])
 	{
 		System.out.println("Hallo World - I am a Dummy");
+		System.out.println("A really stupid one");
 	}
 }
 
--- Dummy.py
+++ Dummy.py
@@ -2,3 +2,7 @@
 ## Im adding a comment for a test
 def functionDummy(h):
     pass
+
+
+def functionDummy2(h):
+    pass
